de3053a57d38614774a7ae0addb2d71ceaede521
Ivan Guerra <ijguerra@correcol.com>
2024-10-03 16:36:15 -0500

    Pipes

--- pipes/src/app/principal/numeros/numeros.component.ts
+++ pipes/src/app/principal/numeros/numeros.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-numeros',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatMenuModule],
   templateUrl: './numeros.component.html',
   styleUrl: './numeros.component.scss'
 })
 export class NumerosComponent {
 
+  totalVentas: number = 25555555555.245355;
+  porcentajeVentas: number = 0.56;
+
+
+  constructor() {
+  }
 }
--- pipes/src/app/principal/otros/otros.component.ts
+++ pipes/src/app/principal/otros/otros.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-otros',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatMenuModule],
   templateUrl: './otros.component.html',
   styleUrl: './otros.component.scss'
 })
-export class OtrosComponent {
 
+export class OtrosComponent {
+  nombre: string = "Carmen";
+  genero: string = "femenino";
+
+  usuarios: string[] = ['Carlos', 'Roberto', 'Marcela'];
+
+  bienvenidoMapa = {
+    'masculino': 'Bienvenido',
+    'femenino': 'Bienvenida'
+  }
+
+  usuariosMapa = {
+    '=0': 'No hay Usuarios en Linea',
+    '=1': 'Tenemos un Usuario en Linea',
+    '=2': 'Tenemos 2 Usuarios en Linea',
+    'other': 'Tenemos # Usuarios en Linea'
+  }
 }
