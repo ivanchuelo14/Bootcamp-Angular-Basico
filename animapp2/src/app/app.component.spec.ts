3c455de24eb78064d1133a119629b51dee82369d
Ivan Guerra <ijguerra@correcol.com>
2024-10-02 19:33:16 -0500

    AJsute codigo fuente

--- animapp2/src/app/app.component.spec.ts
+++ animapp2/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'animapp' title`, () => {
+  it(`should have the 'animapp2' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('animapp');
+    expect(app.title).toEqual('animapp2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, animapp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, animapp2');
   });
 });
